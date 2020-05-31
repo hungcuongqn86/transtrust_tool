5e2cea9337c51fc3ec188f2a0ac2b7c66d029c03
cuongnh <hungcuongqn86@gmail.com>
2020-05-31 23:18:18 +0700

    initial commit

---
+++ main.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace transtrusttool
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.config_btn = new System.Windows.Forms.Button();
+            this.start_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // config_btn
+            // 
+            this.config_btn.Location = new System.Drawing.Point(0, 0);
+            this.config_btn.Name = "config_btn";
+            this.config_btn.Size = new System.Drawing.Size(75, 23);
+            this.config_btn.TabIndex = 0;
+            this.config_btn.Text = "Config";
+            this.config_btn.UseVisualStyleBackColor = true;
+            this.config_btn.Click += new System.EventHandler(this.config_btn_Click);
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(182, 94);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(75, 56);
+            this.start_btn.TabIndex = 1;
+            this.start_btn.Text = "Start";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 255);
+            this.Controls.Add(this.start_btn);
+            this.Controls.Add(this.config_btn);
+            this.Name = "main";
+            this.Text = "TranstrustTool";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button config_btn;
+        private System.Windows.Forms.Button start_btn;
+    }
+}
+
---
+++ main.cs
@@ -0,0 +1,111 @@
+﻿using ActiveUp.Net.Mail;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using transtrusttool.Utils;
+
+namespace transtrusttool
+{
+    public partial class main : Form
+    {
+        public static Imap4Client imap;
+        public BackgroundWorker worker;
+
+        private SamplesConfiguration _configuration;
+        public main()
+        {
+            InitializeComponent();
+            InitializeSample();
+        }
+
+        protected void InitializeSample()
+        {
+            worker = new BackgroundWorker();
+            worker.DoWork += new DoWorkEventHandler(StartIdleProcess);
+        }
+
+        private void StartIdleProcess(object sender, DoWorkEventArgs e)
+        {
+            if (imap != null && imap.IsConnected)
+            {
+                imap.StopIdle();
+                imap.Disconnect();
+            }
+            if (this.Configuration.Imap4Server != null)
+            {
+                imap = new Imap4Client();
+                imap.NewMessageReceived += new NewMessageReceivedEventHandler(NewMessageReceived);
+                //worker.ReportProgress(1, "Connection...");
+                imap.ConnectSsl(this.Configuration.Imap4Server, 993);
+                //worker.ReportProgress(0, "Login...");
+                imap.Login(this.Configuration.Imap4UserName, this.Configuration.Imap4Password);
+                //worker.ReportProgress(0, "Select 'inbox'...");
+                imap.SelectMailbox("inbox");
+                //worker.ReportProgress(0, "Start idle...");
+                imap.StartIdle();
+            }
+        }
+
+        public static void NewMessageReceived(object source, NewMessageReceivedEventArgs e)
+        {
+            MessageBox.Show("New message received :" + e.MessageCount);
+            //imap4.StopIdle();
+        }
+
+        public SamplesConfiguration Configuration
+        {
+            get
+            {
+                if (_configuration == null)
+                {
+                    _configuration = new SamplesConfiguration();
+
+                    string configFullPath = Utils.Common.GetImagePath(Assembly.GetExecutingAssembly().Location) + @"\" + _configuration.FileName;
+
+                    if (File.Exists(configFullPath))
+                    {
+                        TextReader reader = new StreamReader(configFullPath);
+                        XmlSerializer serialize = new XmlSerializer(typeof(SamplesConfiguration));
+                        _configuration = (SamplesConfiguration)serialize.Deserialize(reader);
+                        reader.Close();
+                    }
+                    else
+                    {
+                        _configuration.SetDefaultValue();
+
+                        ConfigurationForm configForm = new ConfigurationForm(this.Configuration);
+                        configForm.ShowDialog();
+                    }
+                }
+                return _configuration;
+            }
+            set
+            {
+                _configuration = value;
+            }
+        }
+
+        private void config_btn_Click(object sender, EventArgs e)
+        {
+            ConfigurationForm configForm = new ConfigurationForm(this.Configuration);
+            configForm.ShowDialog();
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+            if (worker.IsBusy)
+                worker.CancelAsync();
+
+            worker.RunWorkerAsync();
+        }
+    }
+}
